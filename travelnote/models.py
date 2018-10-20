b27894fa290179d055ce53cc56dd0663d317f859
姜楠 <m18646213062_2@163.com>
2018-10-20 08:53:03 +0800

    1111

--- travelnote/models.py
+++ travelnote/models.py
@@ -41,3 +41,5 @@ class commint(models.Model):
     time = models.DateTimeField(max_length=20)
     tid = models.ForeignKey(to='travelnote', to_field='id', on_delete=models.CASCADE)
     userid = models.ForeignKey(to='users.user', to_field='id',on_delete=models.CASCADE, default=1)
+
+aaaaaaaaa
